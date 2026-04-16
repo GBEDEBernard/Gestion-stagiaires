660e73e7836735eb2847ed8e987d68e399e99770
GBEDE Bernard <gbedebernard60@gmail.com>
2026-04-16 12:23:22 +0100

    mise à jour de mon sidebar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -105,7 +105,7 @@ $homeRoute = Auth::user()->hasRole('etudiant') ? route('student.stage') : route(
                                 </svg>
                             </div>
                             <span class="text-white text-xl font-bold">Suivi</span>
-                            <span class="px-3 py-1 text-white text-xs bg-emerald-500/30 text-emerald-200 font-bold rounded-full ring-1 ring-emerald-800/50">Pro</span>
+                            <span class="px-3 py-1 text-xs bg-emerald-500/30 text-gray-200 font-bold rounded-full ring-1 ring-emerald-800/50">Pro</span>
                         </div>
                         <svg x-bind:class="open ? 'rotate-180' : 'rotate-0'" class="w-5 h-5 text-emerald-300 transform transition-transform duration-300 relative z-10" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
