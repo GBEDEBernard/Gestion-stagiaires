28081c7e7e9484d702d186625268e17f7d362e0b
GBEDE Bernard <gbedebernard60@gmail.com>
2026-06-24 15:06:34 +0100

    ajoue des date dans les reference de l'attestation et correction de certaines chose sur le contenu

--- resources/views/admin/stages/attestation.blade.php
+++ resources/views/admin/stages/attestation.blade.php
@@ -31,7 +31,7 @@
         .a4-container::before {
             content: "";
             position: absolute;
-            top: 50%;
+            top: 53%;
             left: 50%;
             transform: translate(-50%, -50%);
             width: 500px;
